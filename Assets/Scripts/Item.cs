19c6016600047fe15e05e16802df0eec7c2b9d5a
Hyein <ms8370@naver.com>
2020-07-22 01:45:53 +0900

    아이템 스크립트 수정

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -6,7 +6,7 @@ public class Item : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.name == "Bar")
+        if (collision.gameObject.name == "Bar")
         {
             Destroy(this.gameObject);
         }
